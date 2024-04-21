f743f4703e3e3c285bfe86db0a27ff932d13694a
LEE JUNE <96871583+marso34@users.noreply.github.com>
2024-04-21 15:52:58 +0900

    [Silver IV] Title: 큐, Time: 388 ms, Memory: 21328 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Main {
