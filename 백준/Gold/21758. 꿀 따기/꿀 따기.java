19a274a36b37514fbf91ab47a2c51bbe3773ac0d
LEE JUNE <96871583+marso34@users.noreply.github.com>
2024-04-29 21:30:56 +0900

    [Gold V] Title: 꿀 따기, Time: 128 ms, Memory: 14228 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        } // 여기까지 입력
+
+        int max = 0;
+
+        // i, j 벌 위치 
+        for(int i = 0; i < n-1; i++) {
+            for(int j = i+1; j < n; j++) {
+                // 벌통 장소
+                for (int k = 0; k < n; k++) { 
+                    if (i == k || j == k) {
+                        continue;
+                    }
+
+                    /**
+                     * 꿀 양 계산
+                     * 
+                     * i ~ k까지 꿀 + j ~ k까지 꿀 
+                     */
+                    int index1 = i; 
+                    int index2 = j;
+                    
+                    int sum = 2* arr[k] - arr[i] - arr[j]; // 2 * 도착지점 - (두 출발 지점)
+
+                    // 출발지점부터 도착지점 이전 까지 sum
+                    // 원래 목표: 출발지 제외 도착지점까지 sum
+                    while (index1 != k || index2 != k) {
+                        if (index1 != k) {
+                            if (index1 != j) {
+                                sum += arr[index1];
+                            }
+                            
+                            index1 = (i > k) ? index1-1 : index1+1;
+                        }
+
+                        if (index2 != k) {
+                            if (index2 != i) {
+                                sum += arr[index2];
+                            }
+                            
+                            index2 = (j > k) ? index2-1 : index2+1;
+                        }
+                    }
+                    
+                    if (sum > max) {
+                        max = sum;
+                    } 
+                }
+            }
+        }
+        
+        System.out.println(max);
+    } 
+}
