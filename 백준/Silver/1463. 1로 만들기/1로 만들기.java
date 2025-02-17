fd303b938a0a3c947b8c75d0cb26fa0836f19e47
LEE JUNE <mars102aq@gmail.com>
2025-02-17 16:17:38 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18128 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -11,21 +11,15 @@ class Main {
         dp[n-1] = 1;
 
         for (int i = n-2; i > 0; i--) {
-            int temp = dp[i+1] + 1;
-
-            if (i * 3 < n && i * 2 < n) {
-                dp[i] = Math.min(dp[i * 3], dp[i * 2]) + 1;
-                dp[i] = Math.min(dp[i], temp);
-                continue;
-            } else if (i * 3 <= n) {
-                dp[i] = Math.min(dp[i * 3] + 1, temp);
-                continue;
-            } else if (i * 2 <= n) {
-                dp[i] = Math.min(dp[i * 2] + 1, temp);
-                continue;
+            dp[i] = dp[i+1] + 1;
+            
+            if (i * 3 <= n) {
+                dp[i] = Math.min(dp[i * 3] + 1, dp[i]);
             }
-
-            dp[i] = temp;
+            
+            if (i * 2 <= n) {
+                dp[i] = Math.min(dp[i * 2] + 1, dp[i]);
+            }
         }
 
         System.out.println(dp[1]);
