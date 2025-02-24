a5ca1e7b86acfb64c89e4a31608c11858c3ee5f0
LEE JUNE <mars102aq@gmail.com>
2025-02-24 16:42:40 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1420 ms, Memory: 115600 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        List<TreeSet<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            list.add(new TreeSet<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list.get(x).add(y);
+            list.get(y).add(x);
+        }
+
+        bfs(list, r);
+    }
+
+    public static void bfs(List<TreeSet<Integer>> list, int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        int[] visited = new int[list.size()-1];
+        int cnt = 1;
+
+        queue.offer(v);
+        visited[v-1] = cnt++;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int i : list.get(cur)) {
+                if (visited[i-1] == 0) {
+                    visited[i-1] = cnt++;
+                    queue.offer(i);
+                }
+            }
+        }
+
+        for (int i : visited) {
+            System.out.println(i);
+        }
+    }
+}
